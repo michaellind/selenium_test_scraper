b1a6a01974fc1a63e9cacd7c96416c92fc7531d3
Michael <iammichaellind@gmail.com>
2021-02-23 14:53:35 -0500

    Adding two more scraping scripts

---
+++ ryukyu_shimpo_article_scraper.py
@@ -0,0 +1,45 @@
+﻿from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from random import randint
+
+PATH = r"C:\Users\Michael\Desktop\chromedriver.exe"
+
+driver = webdriver.Chrome(PATH)
+
+f = open("ryukyushimpo_links.txt")
+start_urls = [url.strip() for url in f.readlines()]
+f.close()
+
+output_file = open("ryukyu_shimpo_articles.txt", "w", encoding="utf-8")
+
+for url in start_urls:
+	driver.get(url)
+	time.sleep(randint(4,6))
+
+	# check to see if a Japanese version of the article exists;
+	# if not, move to next iteration of the loop
+	try:
+		go_to_japanese = WebDriverWait(driver, 2).until(
+			EC.presence_of_element_located((By.LINK_TEXT, "Go to Japanese"))
+		)
+	except:
+		print("No translation for " + url)
+		continue
+	
+	english_article = driver.find_element_by_css_selector(".recent.post").text
+
+	go_to_japanese.click()
+	time.sleep(randint(4,6))
+	japanese_article = driver.find_element_by_id("detail").text
+	output_file.write(english_article)
+	output_file.write("\n\n")
+	output_file.write(japanese_article)
+	output_file.write("\n\n")
+
+output_file.close()
+
+driver.quit()
---
+++ ryukyu_shimpo_links.py
@@ -0,0 +1,31 @@
+﻿from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from random import randint
+
+PATH = r"C:\Users\Michael\Desktop\chromedriver.exe"
+
+driver = webdriver.Chrome(PATH)
+
+i = 197 # set to page to count down from
+
+output_file = open("ryukyushimpo_links.txt", "w", encoding="utf-8")
+
+while i > 0:
+	driver.get("http://english.ryukyushimpo.jp/page/" + str(i))
+	time.sleep(randint(2, 7))
+	article_links = driver.find_element_by_id("content").find_elements_by_tag_name("a")
+	for article_link in article_links:
+		href = article_link.get_attribute("href")
+		if href is not None:
+			output_file.write(href)
+			output_file.write("\n")
+	i = i - 1
+	time.sleep(randint(2, 7))
+
+output_file.close()
+
+driver.quit()
